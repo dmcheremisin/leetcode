363c181a841ad24fba544b6d23147f7b0da7fd54
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2022-04-02 00:03:24 +0300

    Longest substring

--- src/main/java/org/train/leetcode/$0003LongestSubstring.java
+++ src/main/java/org/train/leetcode/$0003LongestSubstring.java
@@ -7,13 +7,14 @@ class $0003LongestSubstring {
 
     public static int lengthOfLongestSubstring(String s) {
         int length = s.length();
-        if (length <= 1) return length;
+        if (length <= 1)
+            return length;
 
         int longest = 0;
         Map<Character, Integer> map = new HashMap<>();
         int left = 0, right = 0, currentLongest = 0;
-        while(left < length && right < length) {
-            while(right < length) {
+        while (left < length && right < length) {
+            while (right < length) {
                 Character aChar = s.charAt(right);
                 if (!map.containsKey(aChar) || map.get(aChar) < left) {
                     map.put(aChar, right);
@@ -22,7 +23,8 @@ class $0003LongestSubstring {
                     longest = Math.max(longest, currentLongest);
                     currentLongest = right - left; // {a:0, b:1, c:2, d:3, b:4, e:5} => 4 - 1 = 3
                     map.put(aChar, right);
-                    left++; right++;
+                    left++;
+                    right++;
                     break;
                 }
                 currentLongest++;
@@ -30,6 +32,27 @@ class $0003LongestSubstring {
             }
         }
         longest = Math.max(longest, currentLongest);
+
+        return longest;
+    }
+
+    public static int lengthOfLongestSubstring2(String s) {
+        int length = s.length();
+        if (length <= 1)
+            return length;
+
+        int longest = 0, left = 0;
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int right = 0; right < length; right++) {
+            Character current = s.charAt(right);
+            Integer position = map.get(current);
+            if (position != null && position >= left)
+                left = position + 1;
+
+            map.put(current, right);
+            longest = Math.max(longest, right - left + 1);
+        }
 
         return longest;
     }
--- src/test/java/org/train/leetcode/$0003LongestSubstringTest.java
+++ src/test/java/org/train/leetcode/$0003LongestSubstringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.train.leetcode.$0003LongestSubstring.lengthOfLongestSubstring;
+import static org.train.leetcode.$0003LongestSubstring.lengthOfLongestSubstring2;
 
 class $0003LongestSubstringTest {
 
@@ -20,5 +21,20 @@ class $0003LongestSubstringTest {
     @Test
     void test3LengthOfLongestSubstring() {
         assertEquals(3, lengthOfLongestSubstring("abcabcbb"));
+    }
+
+    @Test
+    void test4LengthOfLongestSubstring() {
+        assertEquals(3, lengthOfLongestSubstring2("pwwkew"));
+    }
+
+    @Test
+    void test5LengthOfLongestSubstring() {
+        assertEquals(2, lengthOfLongestSubstring2("au"));
+    }
+
+    @Test
+    void test6LengthOfLongestSubstring() {
+        assertEquals(3, lengthOfLongestSubstring2("abcabcbb"));
     }
 }
